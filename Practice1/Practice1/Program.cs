a42b13991ca7dd184e1dabd029772bed57cd0444
Reshma <61983235+resh16@users.noreply.github.com>
2021-08-03 10:08:04 +0530

    Add files via upload

---
+++ Practice1/Practice1/Program.cs
@@ -0,0 +1,33 @@
+﻿//1.Create a static Class and access the methods in the main program
+//2. Create a normal instance class but have few static methods and
+//access them from the main program 
+//3.  In a class have static properties and use them in the methods sections. 
+
+class Practice
+{
+    static void Main()
+    {
+        Employee.EmployeeDetails();
+
+        System.Console.WriteLine(Employee.EmpName);
+        System.Console.WriteLine(Employee.EmpId);
+        System.Console.WriteLine(Employee.companyName);
+
+        System.Console.ReadKey();
+
+    }
+}
+
+static class Employee
+{
+    public static string EmpName = "Reshma";
+    public static int EmpId = 1103;
+    public static string companyName = "Gislen Software Pvt.Ltd";
+
+    public static void EmployeeDetails()
+    {
+        System.Console.WriteLine("The Details of Employee...");
+
+    }
+
+} 
---
+++ Practice3/ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿public class car
+{
+    private string CarColor;
+    public string carColor
+    {
+        set
+        {
+            CarColor = value;
+        }
+        get
+        {
+            return CarColor;
+        }
+    }
+
+    private string CarModel;
+    public string carModel
+    {
+        set
+        {
+            CarModel = value;
+
+        }
+        get
+        {
+            return CarModel;
+        }
+    }
+
+}
