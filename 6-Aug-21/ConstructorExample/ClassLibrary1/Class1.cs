38bbe65d99a9ff199ac27141d00ba810c90e38b0
Reshma <61983235+resh16@users.noreply.github.com>
2021-08-09 08:17:00 +0530

    Add files via upload

---
+++ 6-Aug-21/ConstructorExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class SalaryAccount
+{
+    public string AccountId;
+    public string Name;
+    public int Balance;
+    
+}
+
+public class SalaryOrDemantAccount:SalaryAccount
+{
+    public string PanCardNumber;
+
+
+    
+
+    public SalaryOrDemantAccount(string accountId, string name, int balance, string panCardNumber="")
+    {
+        this.AccountId = accountId;
+        this.Name = name;
+        this.Balance = balance;
+        this.PanCardNumber = panCardNumber;
+        if(panCardNumber == "")
+        {
+            Console.WriteLine("SalaryAccount Created by : " + Name);
+        }
+        Console.WriteLine("Salary and Demant Account Created by : " + Name);
+    }
+}
