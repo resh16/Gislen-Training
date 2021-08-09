0726e1448b750316c7e7b71ec7886d1fd78b584c
Reshma <61983235+resh16@users.noreply.github.com>
2021-08-09 08:46:19 +0530

    Add files via upload

---
+++ 6-Aug-21/C# Assignment/ConstructorExample/Program.cs
@@ -0,0 +1,9 @@
+﻿public class program
+{
+    static void Main()
+    {
+        SalaryOrDemantAccount obj1 = new SalaryOrDemantAccount("9876 5432 1263 3276", "Monica", 20000);
+        SalaryOrDemantAccount obj2 = new SalaryOrDemantAccount("9765 2347 5438 2112", "Chandler", 30000,"ABJK4563WF");
+        System.Console.ReadKey();
+    }
+}
---
+++ 6-Aug-21/C# Assignment/MethodHiding/Program.cs
@@ -0,0 +1,21 @@
+﻿class program
+{
+    static void Main()
+    {
+        Base_class obj1 = new Base_class();
+        Base_class obj2 = new Derived_class();
+        Derived_class obj3 = new Derived_class();
+
+        obj1.writeNum(28);
+        obj1.writeStr("Rachel");
+
+        obj2.writeNum(30);
+        obj2.writeStr("Ross");
+
+        obj3.writeNum(29);
+        obj3.writeStr("Joey");
+
+        System.Console.ReadKey();
+
+    }
+}
