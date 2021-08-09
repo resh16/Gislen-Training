655c7c867732d3c1fcd04db218cf6a8d8e5192da
Reshma <61983235+resh16@users.noreply.github.com>
2021-08-09 08:45:18 +0530

    Add files via upload

---
+++ 5-Aug-21/C# Assignment/Problem 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class problem
+{
+    public static void Main()
+    {
+        Computer cs1 = new Computer("512GB", "i3");
+        Laptop lp1 = new Laptop("8GB", "3Hrs", "i5");
+        HeadPhones hp1 = new HeadPhones("Yes",EnumerationExample.ConnectionType.Bluetooth, "3hrs");
+        HeadPhones hp2 = new HeadPhones("No",EnumerationExample.ConnectionType.Wired);
+
+        Mouse m1 = new Mouse("Optical");
+        DocStation ds1 = new DocStation(5);
+
+
+        cs1.configuration();
+        cs1.Warranty("3 years");
+
+        lp1.configuration();
+        lp1.Warranty("2 years");
+
+        hp1.configuration();
+        hp1.Warranty("6 months");
+
+        hp2.configuration();
+        hp2.Warranty("No Warranty");
+
+        m1.configuration();
+        m1.Warranty("3 months");
+
+        ds1.configuration();
+        ds1.Warranty("3 months");
+
+
+        Console.ReadKey();
+    }
+}
---
+++ 5-Aug-21/C# Assignment/Problem 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Student
+{
+    public int studentId;
+    public string studentName;
+
+    public Student(int stu_id,string stu_name)
+    {
+        this.studentId = stu_id;
+        this.studentName = stu_name;
+    }
+
+    public int getStu_Id()
+    {
+        return studentId;
+    }
+
+    public string getStu_Name()
+    {
+        return studentName;
+    }
+}
+    class PublicExample
+    {
+        static void Main(string[] args)
+        {
+        Student s1 = new Student(101, "Rachel");
+        Student s2 = new Student(102, "Ross");
+
+        Console.WriteLine("Student ID :" + s1.studentId);
+        Console.WriteLine("Student Name :" + s1.studentName);
+
+
+        Console.WriteLine("Student ID :" + s2.getStu_Id());
+        Console.WriteLine("Student Name :" + s2.getStu_Name());
+
+
+
+
+        Console.ReadKey();
+       
+        }
+        
+    }
+
