60ea2f00df7fa33929c9bf7ecdbe218cfecc529b
Reshma <61983235+resh16@users.noreply.github.com>
2021-08-10 11:01:52 +0530

    Add files via upload

---
+++ 9-Aug-21/DoublyLinkedList-Implementation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DLLCaller();
+            Console.ReadLine();
+        }
+        static void DLLCaller()
+        {
+            HelperDLL objHelper = new HelperDLL();
+            DoubleLinkedList myLinkedList = new DoubleLinkedList();
+
+            objHelper.InsertLast(myLinkedList, 20);
+            objHelper.InsertLast(myLinkedList, 50);
+            objHelper.InsertLast(myLinkedList, 40);
+            objHelper.InsertLast(myLinkedList, 25);
+            objHelper.InsertLast(myLinkedList, 63);
+            objHelper.PrintList(myLinkedList);
+            Console.WriteLine(" ");
+            objHelper.DeleteNodebyKey(myLinkedList, 25);
+            objHelper.PrintList(myLinkedList);
+            Console.WriteLine(" ");
+            objHelper.ReversalLinkedList(myLinkedList);
+            objHelper.PrintList(myLinkedList);
+            Console.WriteLine(" ");
+
+
+        }
+    }
+}
