9ec8b8c20f5571ae7c3cac43805f4b8af2772313
Reshma <61983235+resh16@users.noreply.github.com>
2021-08-10 10:38:07 +0530

    Add files via upload

---
+++ 9-Aug-21/LinkedListQueue-Implementation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace QueueImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueueCaller();
+
+            Console.ReadLine();
+        }
+
+        public static void LinkedListQueueCaller()
+        {
+            LinkedListQueue myQueue = new LinkedListQueue();
+
+            myQueue.Enqueue(10);
+            myQueue.Enqueue(20);
+            myQueue.Enqueue(30);
+            myQueue.Enqueue(40);
+            myQueue.Enqueue(50);
+
+            myQueue.PrintQueue();
+
+            myQueue.Dequeue();
+            myQueue.Enqueue(60);
+
+            myQueue.PrintQueue();
+            myQueue.Dequeue();
+            myQueue.PrintQueue();
+
+
+
+        }
+    }
+}
