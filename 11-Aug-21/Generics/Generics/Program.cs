c3d84440b4874def9ec2129f2eea5155814f2e26
Reshma <61983235+resh16@users.noreply.github.com>
2021-08-12 10:13:28 +0530

    Add files via upload

---
+++ 11-Aug-21/Generics/Generics/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpoyeeDetails<int, string> employeeDetails = new EmpoyeeDetails<int, string>();
+            employeeDetails.AddEmployee(1002, "Chandler");
+            employeeDetails.AddEmployee(1004, "Monica");
+            employeeDetails.AddEmployee(1001, "Rachel");
+            employeeDetails.AddEmployee(1003, "Joey");
+            employeeDetails.AddEmployee(1006, "Ross");
+            employeeDetails.RemoveEmployee(1002);
+            employeeDetails.SortEmployee();
+            employeeDetails.AddEmployee(1005, "Pheobe");
+            employeeDetails.display();
+            Console.ReadKey();
+        }
+    }
+
+    class EmpoyeeDetails<k,v>
+    {
+        Dictionary<int, string> employee = new Dictionary<int, string>();
+            
+
+        public void AddEmployee(int id,string name)
+        {
+            employee.Add(id, name);
+        }
+
+        public void RemoveEmployee(int id)
+        {
+            employee.Remove(id);
+        }
+
+        public void SortEmployee()
+        {
+            var list = employee.Keys.ToList();
+            list.Sort();
+
+            foreach(var key in list)
+            {
+                Console.Write(key + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void display()
+        {
+            foreach(var i in employee)
+            {
+                Console.WriteLine("Key : " + i.Key + ", Value : " + i.Value);
+            }
+        }
+    }
+}
---
+++ 11-Aug-21/Linq/Linq/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BankBranch> bankBranches = new List<BankBranch>();
+
+            bankBranches.Add(new BankBranch { BankName = "SBI", Area = "Alwarthirunagar" });
+            bankBranches.Add(new BankBranch { BankName = "Axis", Area = "Valasaravakkam" });
+            bankBranches.Add(new BankBranch { BankName = "PNB", Area = "Vadapalani" });
+
+            List<BankManager> bankManagers = new List<BankManager>();
+
+            bankManagers.Add(new BankManager { ManagerName = "Ross", BankName = "SBI", age = 27, maxPoint = 88 });
+            bankManagers.Add(new BankManager { ManagerName = "John", BankName = "Axis", age = 37, maxPoint = 92 });
+            bankManagers.Add(new BankManager { ManagerName = "Rithu", BankName = "Axis", age = 42, maxPoint = 83 });
+            bankManagers.Add(new BankManager { ManagerName = "Dani", BankName = "PNB", age = 20, maxPoint = 75 });
+
+            foreach (BankManager i in bankManagers)
+            {
+                Console.WriteLine(i.BankName + " branch manager is " + i.ManagerName);
+            }
+
+            Console.WriteLine();
+            //-------
+            var bankmanagersAbove30 = bankManagers.FindAll(x => x.age > 30);
+
+            foreach (var age in bankmanagersAbove30)
+            {
+                Console.WriteLine("The managers name whose age is above 30: " + age.ManagerName);
+            }
+
+            Console.WriteLine();
+            //----------
+            var max_point = bankManagers.Max(point => point.maxPoint);
+
+            var bankmanagersMaxPoint = bankManagers.FindAll(x => x.maxPoint == max_point);
+
+            foreach (var maxpoint in bankmanagersMaxPoint)
+            {
+                Console.WriteLine("The bank manager who has maximum point: " + maxpoint.ManagerName);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class BankBranch
+    {
+        public string BankName { get; set; }
+        public string Area { get; set; }
+    }
+
+    class BankManager
+    {
+        public string ManagerName { get; set; }
+        public int age { get; set; }
+        public int maxPoint { get; set; }
+        public string BankName { get; set; }
+
+    }
+}
